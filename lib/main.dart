9490b041178b6d01baafab4a7ec7b8e4c52b33b2
ozge <20210808023@ogr.akdeniz.edu.tr>
2023-07-31 13:45:28 +0300

    User avatars added

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,6 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter_boilerplate/screens/login/login.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
-import 'constants.dart';
-import 'screens/home/home_page.dart';
-import 'services/product_service/providers/product_provider.dart';
-
+import 'package:flutter_boilerplate/users_page.dart';
 
 void main() {
   HttpOverrides.global = MyHttpOverrides();
@@ -26,30 +20,14 @@ class MyHttpOverrides extends HttpOverrides {
   }
 }
 
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData.dark().copyWith(
-          scaffoldBackgroundColor: bgColor,
-          textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)
-              .apply(bodyColor: Colors.white),
-          canvasColor: secondaryColor,
-        ),
-        initialRoute: LoginPage.routeName,
-        routes: {
-          MyHomePage.routeName: (context) =>
-              const MyHomePage(title: 'Flutter Demo Home Page'),
-          LoginPage.routeName: (context) => const LoginPage(),
-
-        },
-      ),
+      debugShowCheckedModeBanner: false,
+      home: UsersPage(),
     );
-
   }
 }
