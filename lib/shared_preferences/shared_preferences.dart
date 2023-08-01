54dece5a0f5c63debc3a0dfb156f668047214818
Özgür Cansız <ozgurcansizz@outlook.com>
2023-08-01 13:52:24 +0300

    .

--- lib/shared_preferences/shared_preferences.dart
+++ lib/shared_preferences/shared_preferences.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class SPService {
   static const _key = "token";
